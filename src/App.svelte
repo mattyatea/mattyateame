ff9fd0eaf09d213cb4005172fb807cbe0b0460e3
mattyatea <56515516+mattyatea@users.noreply.github.com>
2025-08-27 03:35:06 +0900

    色々をアップデート

--- src/App.svelte
+++ src/App.svelte
@@ -51,52 +51,50 @@ function getStarRating(star: number): string {
 </script>
 
 <main>
-    <div class="flex flex-col gap-y-4 md:w-2/4 mx-auto">
-            <div class="bg-emerald-100 p-4 rounded-2xl shadow-[8px_12px_23px_-10px_#052E164d]">
-                <h1 class="text-3xl">プロフィール</h1>
+    <div class="flex flex-col lg:w-9/12 w-full mx-auto">
+            <div class="bg-gray-300 dark:bg-slate-500 p-4 rounded-2xl ">
                 <div class="flex gap-x-4 flex-col sm:flex-row gap-y-4 sm:gap-x-8 sm:gap-y-0 items-center">
                     <img alt="まっちゃてぃー。のアイコン アニメ調の女の子の画像" src="https://mattyaski.co/avatar/@mattyatea" class="rounded-2xl w-48 h-48">
-                    <div class="flex flex-col items-left">
+                    <div class="flex flex-col items-left dark:text-gray-200">
                         <span>名前：まっちゃてぃー。</span>
                         <span>生年月日：2008/07/03</span>
-                        <span>株式会社9ineverse 執行役員</span>
                     </div>
                 </div>
 
             </div>
 
-        <div class="flex flex-col w-full mt-4 bg-emerald-100 p-8 rounded-2xl shadow-[8px_12px_23px_-10px_#052E164d]">
+        <div class="flex flex-col w-full mt-4 bg-gray-300 dark:bg-slate-500 p-8 rounded-2xl">
             <h1 class="text-3xl">技術スタック</h1>
 
             <div class="my-2">
-                <h1 class="text-base mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(5)}</span> <br>他者にも教えることができます ✅</h1>
-                <h1 class="text-base mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(4)}</span> <br>大規模プロジェクトで活躍できます ✅</h1>
-                <h1 class="text-base mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(3)}</span> <br>小〜中規模のプロジェクトでなら活躍できます ✅</h1>
-                <h1 class="text-base mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(2)}</span> <br>基本的なことは理解している ✅</h1>
-                <h1 class="text-base mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(1)}</span> <br>チュートリアル ✅</h1>
+                <h1 class="text-base dark:text-gray-200 mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(5)}</span> <br>他者にも教えることができます ✅</h1>
+                <h1 class="text-base dark:text-gray-200 mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(4)}</span> <br>大規模プロジェクトで活躍できます ✅</h1>
+                <h1 class="text-base dark:text-gray-200 mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(3)}</span> <br>小〜中規模のプロジェクトでなら活躍できます ✅</h1>
+                <h1 class="text-base dark:text-gray-200 mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(2)}</span> <br>基本的なことは理解している ✅</h1>
+                <h1 class="text-base dark:text-gray-200 mb-3 sm:text-xl"><span class="text-yellow-500">{getStarRating(1)}</span> <br>チュートリアル ✅</h1>
             </div>
 
                 {#each stacks as stack}
-                    <div class="flex justify-between items-center text-base">
+                    <div class="flex justify-between items-center text-base dark:text-gray-200">
                         <span>{stack.name}</span>
                         <span class="text-yellow-500">{getStarRating(stack.star)}</span>
                     </div>
                 {/each}
         </div>
 
-        <div class="flex flex-col w-full mt-4 bg-emerald-100 p-8 rounded-2xl shadow-[8px_12px_23px_-10px_#052E164d] c">
+        <div class="flex flex-col w-full mt-4 bg-gray-300 dark:bg-slate-500 p-8 rounded-2xl">
             <ol class="relative border-s border-gray-200 ">
                 <TimelineItem
                         title="nanasi-apps.xyz"
                         date="2021/11/06 ~ 現在"
-                        description='<p class="text-base text-gray-800">私自身が知見を得たことを発信していくためのブログです。</p>'
+                        description='<p class="text-base text-gray-800 dark:text-gray-200">私自身が知見を得たことを発信していくためのブログです。</p>'
                 />
 
                 <TimelineItem
                         title="mkey.nanasi-apps.xyz"
                         date="2022/05/06 ~ 2022/07/24"
                         description='
-                        <p class="text-base text-gray-800">
+                        <p class="text-base text-gray-800 dark:text-gray-200">
                             サーバーに興味があったので、当時自分の中で流行っていた Misskey のサーバーを立てました。
                             <br>
                             この経験で、S3互換ファイルサーバーや、Linuxへの理解など、ここには書ききれないほどの知見を得ることができました。</p>'
@@ -105,45 +103,33 @@ function getStarRating(star: number): string {
                 <TimelineItem
                         title="mattyaski.co"
                         date="2022/12/24 ~ 現在"
-                        description='
-                        <p class="text-base text-gray-800">
-
-                        </p>'
                 />
 
                 <TimelineItem
                         title="PrisMisskey.space"
                         date="2023/09/24 ~ 現在"
-                        description='
-                        <p class="text-base text-gray-800">
-
-                        </p>'
                 />
 
                 <TimelineItem
                         title="Misskey-dev Member"
                         date="2023/10/19 ~ 現在"
-                        description='
-                        <p class="text-base text-gray-800">
-
-                        </p>'
                 />
 
                 <TimelineItem
                         title="S高等学校 - 入学"
                         date="2024/04/01 ~ 現在"
-                        description='
-                        <p class="text-base text-gray-800">
-
-                        </p>'
                 />
 
                 <TimelineItem
                         title="株式会社9ineverse - 執行役員"
-                        date="2024/08/01 ~ 現在"
+                        date="2024/08/01 ~ 2025/08/01"
+                />                
+                <TimelineItem
+                        title="株式会社****** - アルバイト"
+                        date="2025/01/05 ~ 現在"
                         description='
-                        <p class="text-base text-gray-800">
-
+                        <p class="text-base text-gray-800 dark:text-gray-200">
+                            Vue.jsや、Java、Next.js等を用いた開発を行っています。
                         </p>'
                 />
             </ol>
@@ -151,10 +137,8 @@ function getStarRating(star: number): string {
         </div>
 
         <div class="mx-auto">
-            <p>Copyright © 2024 mattyatea All rights reserved. </p>
+            <p>Copyright © 2025 mattyatea All rights reserved. </p>
         </div>
     </div>
 </main>
 
-<style>
-</style>
--- src/lib/TimelineItem.svelte
+++ src/lib/TimelineItem.svelte
@@ -1,18 +1,18 @@
 <script>
     export let title;
     export let date;
-    export let description;
+    export let description = "";
 </script>
 
 <li class="mb-10 ms-6">
-  <span class="absolute flex items-center justify-center w-6 h-6 bg-emerald-200 rounded-full -start-3 ring-8 ring-emerald-100 ">
+  <span class="absolute flex items-center justify-center w-6 h-6 bg-gray-300 rounded-full -start-3 ring-8 ring-gray-200 ">
     <svg class="w-2.5 h-2.5 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
       <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
     </svg>
   </span>
-    <h3 class="flex items-center mb-1 text-lg">
+    <h3 class="flex items-center mb-1 text-lg dark:text-gray-200">
         {title}
     </h3>
-    <time class="block mb-2 text-sm font-normal leading-none text-gray-700">{date}</time>
+    <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-300">{date}</time>
     {@html description}
 </li>
